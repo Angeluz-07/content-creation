46bc80ad66ba9ee2e20e401655b7b8a8408cfdef
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-04-02 12:47:47 -0500

    update docker commands to mount local volume

--- denoise_voice_records/docker_bash_img.bat
+++ denoise_voice_records/docker_bash_img.bat
@@ -1,1 +1,5 @@
-docker run -it angeluz07/content-creation-img:0.0.1 bash
+# bash into running instance
+docker exec -it jovial_goldstine bash
+
+# bash into fresh instance
+# docker run  -it angeluz07/content-creation-img:0.0.1 bash
