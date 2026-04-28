505b4dc6533f6e64086ceae5f1948f972eb1888f
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-04-28 11:02:34 -0500

    add tailwind, daisyui, temporary version of tailwind/vite

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -6,6 +6,11 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-primary">Button</button>
+
 </template>
 
 <style scoped></style>
