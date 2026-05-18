857e54b27f9df376ebb5fbe52234b92248d00942
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-05-18 12:49:23 -0500

    extract component for shortprod params

---
+++ ui/src/views/ShortProductionParamsForm.vue
@@ -0,0 +1,156 @@
+<script setup lang="ts">
+import { useApi } from '@/composables/useApi'
+import type { ShortProductionParams } from '../types/config'
+import { toShortProductionParamsPayload } from '../mappers/config'
+import { reactive, ref, onMounted } from 'vue'
+import ModalVideoPlayer from './ModalVideoPlayer.vue'
+
+const WATERMARK_TEXT = import.meta.env.VITE_WATERMARK_TEXT
+
+const form = reactive<ShortProductionParams>({
+  inputFileName: '',
+  watermarkText: WATERMARK_TEXT,
+  debugVideoFrame: true,
+  hookText: 'test',
+  frameTs: '00:00:03',
+  fontName: 'GoogleSans-Medium',
+})
+
+const fontList = ref([
+  'GoogleSans-Medium',
+  'Anton-Regular',
+  'Bangers-Regular',
+  'CascadiaCode',
+  'LuckiestGuy-Regular',
+  'Montserrat-Bold',
+  'PassionOne-Regular',
+  'ProtestStrike-Regular',
+])
+
+const fileNames = ref([])
+const { loading: isSubmitting, post: sendForm } = useApi()
+const { loading: loadingVideoFileNames, get: getVideoFileNames } = useApi()
+
+const handleSubmit = async () => {
+  const payload = toShortProductionParamsPayload(form)
+  const { success, data } = await sendForm('/produce-short', payload)
+  //refreshImage()
+  //refreshVideo()
+}
+
+async function loadVideoFileNames() {
+  const { data } = await getVideoFileNames('/video/raws/')
+
+  if (data && data.values.length > 0) {
+    fileNames.value = data.values
+    form.inputFileName = data.values[0]
+  }
+}
+
+onMounted(async () => {
+  loadVideoFileNames()
+})
+</script>
+
+<template>
+  <div class="card w-full bg-base-100 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title mb-4">Params</h2>
+
+      <form @submit.prevent="handleSubmit" class="space-y-4">
+        <div class="form-control w-full">
+          <label class="label">
+            <span class="label-text font-semibold">Input Filename</span>
+          </label>
+          <div class="grid grid-cols-6 gap-4">
+            <select
+              v-model="form.inputFileName"
+              class="select select-bordered w-full col-span-5"
+              required
+            >
+              <option value="" disabled selected>Select a file</option>
+              <!-- Loop through the list of strings -->
+              <option v-for="fileName in fileNames" :key="fileName" :value="fileName">
+                {{ fileName }}
+              </option>
+            </select>
+            <ModalVideoPlayer :fileName="form.inputFileName"></ModalVideoPlayer>
+          </div>
+        </div>
+
+        <div class="form-control w-full">
+          <label class="label">
+            <span class="label-text font-semibold">Font</span>
+          </label>
+          <!-- Use v-model for binding and select tag instead of input -->
+          <select v-model="form.fontName" class="select select-bordered w-full" required>
+            <option value="" disabled selected>Select a font</option>
+            <!-- Loop through the list of strings -->
+            <option v-for="fontName in fontList" :key="fontName" :value="fontName">
+              {{ fontName }}
+            </option>
+          </select>
+        </div>
+
+        <div class="form-control w-full">
+          <label class="label">
+            <span class="label-text font-semibold">Watermark text</span>
+          </label>
+          <input
+            v-model="form.watermarkText"
+            type="text"
+            placeholder="@theExample"
+            class="input input-bordered w-full"
+            required
+          />
+        </div>
+        <div class="form-control w-full">
+          <label class="label">
+            <span class="label-text">Hook Text</span>
+          </label>
+          <input
+            v-model="form.hookText"
+            type="text"
+            placeholder="Escribe el hook aquí"
+            class="input input-bordered w-full"
+          />
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div class="form-control w-full">
+            <label class="label">
+              <span class="label-text">Frame to use (HH:MM:SS)</span>
+            </label>
+            <input
+              v-model="form.frameTs"
+              type="text"
+              pattern="^([0-9]{2}:){2}[0-9]{2}$"
+              placeholder="00:00:00"
+              class="input input-bordered w-full"
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-col md:flex-row gap-6 pt-4">
+          <div class="form-control">
+            <label class="label cursor-pointer justify-start gap-3">
+              <input v-model="form.debugVideoFrame" type="checkbox" class="toggle toggle-primary" />
+              <span class="label-text">Debug Video Frame</span>
+            </label>
+          </div>
+        </div>
+
+        <div class="card-actions justify-end mt-6">
+          <button
+            type="submit"
+            :class="{ 'pointer-events-none': isSubmitting }"
+            :tabindex="isSubmitting ? -1 : 0"
+            class="btn btn-primary w-full md:w-auto"
+          >
+            <span v-if="isSubmitting" class="loading loading-spinner"></span>
+            {{ isSubmitting ? 'Procesando...' : 'Enviar' }}
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
--- ui/src/views/ShortProductionSection.vue
+++ ui/src/views/ShortProductionSection.vue
@@ -1,66 +1,9 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import { ref, onMounted, computed } from 'vue'
-import type { ShortProductionParams } from '../types/config'
-import { toShortProductionParamsPayload } from '../mappers/config'
 import api from '@/api/client'
-import ModalVideoPlayer from './ModalVideoPlayer.vue'
+import ShortProductionParamsForm from './ShortProductionParamsForm.vue'
 
-const WATERMARK_TEXT = import.meta.env.VITE_WATERMARK_TEXT
-
-const form = reactive<ShortProductionParams>({
-  inputFileName: '',
-  watermarkText: WATERMARK_TEXT,
-  debugVideoFrame: true,
-  hookText: 'test',
-  frameTs: '00:00:10',
-  fontName: 'GoogleSans-Medium',
-})
-
-const fileNames = ref([])
-
-onMounted(async () => {
-  try {
-    const { data } = await api.get('/video/raws/')
-    fileNames.value = data.values
-
-    // // Optional: Pre-select the first item if the list isn't empty
-    if (fileNames.value.length > 0) {
-      form.inputFileName = fileNames.value[0]
-    }
-  } catch (error) {
-    console.error('Failed to load options:', error)
-  }
-})
-const fontList = ref([
-  'GoogleSans-Medium',
-  'Anton-Regular',
-  'Bangers-Regular',
-  'CascadiaCode',
-  'LuckiestGuy-Regular',
-  'Montserrat-Bold',
-  'PassionOne-Regular',
-  'ProtestStrike-Regular',
-])
-
-const isLoading = ref(false)
-
-const handleSubmit = async () => {
-  isLoading.value = true
-  try {
-    console.log('Datos a enviar:', form)
-
-    const payload = toShortProductionParamsPayload(form)
-    const { data } = await api.post('/produce-short', payload)
-    refreshImage()
-    refreshVideo()
-    // console.log(data)
-  } catch (error) {
-    console.error('Error al enviar:', error)
-  } finally {
-    isLoading.value = false
-  }
-}
 
 const isLoaded = ref(false)
 const isVideoLoaded = ref(true)
@@ -86,116 +29,12 @@ const refreshVideo = () => {
   const filename = form.inputFileName
   videoUrl.value = `http://localhost:8000/video/${filename}_produced?t=${timestamp}`
 }
-
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto p-4 grid grid-cols-1 md:grid-cols-3 gap-6 items-start">
-    <div class="card w-full bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title mb-4">Params</h2>
-
-        <form @submit.prevent="handleSubmit" class="space-y-4">
-          <div class="form-control w-full">
-            <label class="label">
-              <span class="label-text font-semibold">Input Filename</span>
-            </label>
-            <div class="grid grid-cols-6 gap-4">
-              <select
-                v-model="form.inputFileName"
-                class="select select-bordered w-full col-span-5"
-                required
-              >
-                <option value="" disabled selected>Select a file</option>
-                <!-- Loop through the list of strings -->
-                <option v-for="fileName in fileNames" :key="fileName" :value="fileName">
-                  {{ fileName }}
-                </option>
-              </select>
-              <ModalVideoPlayer :fileName="form.inputFileName"></ModalVideoPlayer>
-            </div>
-          </div>
-
-          <div class="form-control w-full">
-            <label class="label">
-              <span class="label-text font-semibold">Font</span>
-            </label>
-            <!-- Use v-model for binding and select tag instead of input -->
-            <select v-model="form.fontName" class="select select-bordered w-full" required>
-              <option value="" disabled selected>Select a font</option>
-              <!-- Loop through the list of strings -->
-              <option v-for="fontName in fontList" :key="fontName" :value="fontName">
-                {{ fontName }}
-              </option>
-            </select>
-          </div>
-
-          <div class="form-control w-full">
-            <label class="label">
-              <span class="label-text font-semibold">Watermark text</span>
-            </label>
-            <input
-              v-model="form.watermarkText"
-              type="text"
-              placeholder="@theExample"
-              class="input input-bordered w-full"
-              required
-            />
-          </div>
-          <div class="form-control w-full">
-            <label class="label">
-              <span class="label-text">Hook Text</span>
-            </label>
-            <input
-              v-model="form.hookText"
-              type="text"
-              placeholder="Escribe el hook aquí"
-              class="input input-bordered w-full"
-            />
-          </div>
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div class="form-control w-full">
-              <label class="label">
-                <span class="label-text">Frame to use (HH:MM:SS)</span>
-              </label>
-              <input
-                v-model="form.frameTs"
-                type="text"
-                pattern="^([0-9]{2}:){2}[0-9]{2}$"
-                placeholder="00:00:00"
-                class="input input-bordered w-full"
-              />
-            </div>
-          </div>
-
-          <div class="flex flex-col md:flex-row gap-6 pt-4">
-            <div class="form-control">
-              <label class="label cursor-pointer justify-start gap-3">
-                <input
-                  v-model="form.debugVideoFrame"
-                  type="checkbox"
-                  class="toggle toggle-primary"
-                />
-                <span class="label-text">Debug Video Frame</span>
-              </label>
-            </div>
-          </div>
-
-          <div class="card-actions justify-end mt-6">
-            <button
-              type="submit"
-              :class="{ 'pointer-events-none': isLoading }"
-              :tabindex="isLoading ? -1 : 0"
-              class="btn btn-primary w-full md:w-auto"
-            >
-              <span v-if="isLoading" class="loading loading-spinner"></span>
-              {{ isLoading ? 'Procesando...' : 'Enviar' }}
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-
+    
+    <ShortProductionParamsForm></ShortProductionParamsForm>
     <div class="card w-full md:w-fit mx-auto bg-base-100 shadow-xl overflow-hidden">
       <div class="card-body p-4">
         <h2 class="card-title">Vista previa</h2>
