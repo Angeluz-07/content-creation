ee4bed20c89440d4730cbb4bc45d2e4f4cbcb77e
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-05-28 14:20:53 -0500

    add method to download vtt file

--- shorts_production/domain/services/yt_downloader.py
+++ shorts_production/domain/services/yt_downloader.py
@@ -19,18 +19,13 @@ class YTDownloader:
         raw_filepath = str(Path(self.output_path) / f"{output_filename}.mp4")
         file_doesnt_exist = not Path(raw_filepath).is_file()
 
-        if file_doesnt_exist:
-            print("File doesnt exist, downloading...")
-            self._download_segment_from_yt(start_ts, end_ts, url, raw_filepath)
-        elif force_download:
-            print(f"File exist & force_download={force_download}, downloading...")
-            file_to_remove = Path(raw_filepath)
-            file_to_remove.unlink(missing_ok=True)  # deletes file
+        if file_doesnt_exist or force_download:
+            print(
+                f"File doesnt exist or force_download={force_download}, downloading..."
+            )
             self._download_segment_from_yt(start_ts, end_ts, url, raw_filepath)
         else:
-            print(
-                f"Raw file exists & force_download={force_download}, skipping raw download..."
-            )
+            print(f"File exists, force_download={force_download}, skipping download...")
         return raw_filepath
 
     def _download_segment_from_yt(
@@ -43,7 +38,8 @@ class YTDownloader:
             "yt-dlp", url,
             "--external-downloader-args", "ffmpeg:-loglevel error", # hides extra logs
             "--postprocessor-args", "ffmpeg:-loglevel error",  # hides extra logs
-            #"--list-formats", # for debug only 
+            "--force-overwrites",
+            #"--list-formats", # for debug only
             "--no-playlist",
             "--cookies", "my_cookies.txt",
             "--js-runtimes", "node",
@@ -56,6 +52,62 @@ class YTDownloader:
             "--merge-output-format", "mp4",
             "--extractor-args",
             "youtube:player_client=default",
+            #"--verbose",  # for debug only
+        ]
+        # fmt: on
+        try:
+            start_time = time.perf_counter()
+
+            result = subprocess.run(command, check=True, text=True)
+
+            end_time = time.perf_counter()
+            print(f"Elapsed time: {end_time - start_time:.4f} seconds")
+            print("Raw segment downloaded successfully via Subprocess")
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error while downloading with subprocess: {e}")
+            raise e
+        except Exception as e:
+            print(f"General error: {e}")
+            raise e
+
+    def get_vtt(
+        self,
+        url: str,
+        force_download: bool,
+        output_filename: str,
+    ) -> str:
+        raw_filepath = str(Path(self.output_path) / f"{output_filename}.vtt")
+        file_doesnt_exist = not Path(raw_filepath).is_file()
+
+        if file_doesnt_exist or force_download:
+            print(
+                f"File doesnt exist or force_download={force_download}, downloading..."
+            )
+            self._download_vtt(url, raw_filepath)
+        else:
+            print(f"File exists, force_download={force_download}, skipping download...")
+        return raw_filepath
+
+    def _download_vtt(self, url: str, output_path: str):
+        print(f"Starting download vtt (via Subprocess): {url}")
+
+        # fmt: off
+        command = [
+            "yt-dlp", url,
+            "--external-downloader-args", "ffmpeg:-loglevel error", # hides extra logs
+            "--postprocessor-args", "ffmpeg:-loglevel error",  # hides extra logs
+            "--force-overwrites",
+            "--no-playlist",
+            "--cookies", "my_cookies.txt",
+            "--js-runtimes", "node",
+            "--remote-components", "ejs:github",
+            "--write-subs",
+            "--write-auto-sub",
+            "--sub-lang", "es",
+            "--skip-download",
+            "--convert-subs", "vtt",
+            "-o", output_path,
             #"--verbose",  # for debug only
         ]
         # fmt: on
