a86e2115e26bcf5d6101498e222602fd8ec3034e
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-06-05 15:18:48 -0500

    update root data folder

--- production/config.py
+++ production/config.py
@@ -5,14 +5,13 @@ import os
 PROJECT_DIR = Path(__file__).resolve().parent
 
 DATA_DIR =  PROJECT_DIR / ".data"
-#DOWNLOAD_DIR = str(DATA_DIR / "downloads")
 OUTPUT_DIR = str(DATA_DIR / "output_videos")
 TEMP_DIR = str(DATA_DIR / "temp")
 ASSETS_DIR = str(DATA_DIR / "assets")
 TEXT_FONT_PATH = str(DATA_DIR / "assets" / "ProtestStrike-Regular.ttf")
 
-COOKIES_PATH = str(PROJECT_DIR.parent / "cookies.txt")
-DOWNLOAD_DIR  =  str(PROJECT_DIR.parent / "services" / "download" / ".data" / "downloads")
+COOKIES_PATH = str(PROJECT_DIR.parent / ".data" / "cookies.txt")
+DOWNLOAD_DIR  =  str(PROJECT_DIR.parent / ".data" / "downloads")
 
 ENV_DIR = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=ENV_DIR)
