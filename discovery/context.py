6c7af5e46f00f3e17ea75819d961fbe86401f410
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-06-15 15:53:28 -0500

    add download service, mapping functions in discovery

--- discovery/context.py
+++ discovery/context.py
@@ -1,10 +1,11 @@
-from discovery.dbs.qdrant_client import get_qdrant_client
-from discovery.dbs.qdrant_store import QdrantVectorStore
-from discovery.services.embedding import Embedder
-from discovery.services.ingestion import Ingester
-from discovery.services.detection import Detector
-from discovery.services.transcription import GroqAudioTranscriber
-from discovery.config import GROQ_API_KEY
+from dbs.qdrant_client import get_qdrant_client
+from dbs.qdrant_store import QdrantVectorStore
+from services.embedding import Embedder
+from services.ingestion import Ingester
+from services.detection import Detector
+from services.transcription import GroqAudioTranscriber
+from config import GROQ_API_KEY
+from services.download import DownloadService
 
 client = get_qdrant_client()
 collection_name = "moments"  # change to 'moments'
@@ -17,3 +18,5 @@ qvs.ensure_collection_exists()
 transcriber = GroqAudioTranscriber(GROQ_API_KEY)
 ingester = Ingester(vector_store=qvs, embedder=embedder, transcriber=transcriber)
 metal_detector = Detector(vector_store=qvs, embedder=embedder)
+
+download_service = DownloadService()
---
+++ discovery/services/download.py
@@ -0,0 +1,27 @@
+import requests
+from typing import Dict
+
+class DownloadService:
+    def __init__(
+        self,
+    ):
+        self.url = "http://localhost:8002"
+
+    def trigger(self, params: Dict):
+        url = params["url"]
+        start_ts = params["start_segment"]
+        end_ts = params["end_segment"]
+        force_download = params["force_download"]
+        output_filename = params["output_filename"]
+
+        response = requests.post(
+            f"{self.url}/download-segment",
+            json={
+                "url": url,
+                "force_download": force_download,
+                "start_segment": start_ts,
+                "end_segment": end_ts,
+                "output_filename": output_filename,
+            },
+        )
+
