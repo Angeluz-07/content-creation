f2ffb8bb2b611d96d07e992bc0ba1e36f487f7cf
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-06-16 17:02:27 -0500

    add production handler to services in compose

--- services/production/config.py
+++ services/production/config.py
@@ -5,13 +5,10 @@ import os
 PROJECT_DIR = Path(__file__).resolve().parent
 
 DATA_DIR = PROJECT_DIR / ".data"
-ASSETS_DIR = str(DATA_DIR / "assets")
-TEXT_FONT_PATH = str(DATA_DIR / "assets" / "ProtestStrike-Regular.ttf")
 
-COOKIES_PATH = str(PROJECT_DIR.parent / ".data" / "cookies.txt")
-DOWNLOAD_DIR = str(PROJECT_DIR.parent / ".data" / "downloads")
-TEMP_DIR = str(PROJECT_DIR.parent / ".data" / "temp")
-OUTPUT_DIR = str(PROJECT_DIR.parent / ".data" / "output_videos")
+DOWNLOAD_DIR = str(DATA_DIR / "downloads")
+TEMP_DIR = str(DATA_DIR / "temp")
+OUTPUT_DIR = str(DATA_DIR/ "output_videos")
 
 
 MONGO_DB_NAME = "cc_db"
--- services/production/handlers/main.py
+++ services/production/handlers/main.py
@@ -1,12 +1,12 @@
 from faststream import FastStream
 from faststream.redis import RedisRouter, RedisBroker
 from handlers.routes import router
-
+from config import REDIS_URI
 event_router = RedisRouter()
 
 event_router.include_router(router)
 
 
-broker = RedisBroker("redis://localhost:6379/0")
+broker = RedisBroker(REDIS_URI)
 broker.include_router(event_router)
 backend_worker = FastStream(broker)
