eba04c83e220f8b6800da7abc9389443642e7294
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-06-16 17:41:21 -0500

    revert ui as microservice

--- ui/src/views/DownloadSection.vue
+++ ui/src/views/DownloadSection.vue
@@ -5,7 +5,7 @@ import TaskDownloads from './TaskDownloads.vue';
 
 <template>
   <div class="max-w-7xl mx-auto p-4 grid md:grid-cols-5 gap-8 items-start">
-    <DownloadForm></DownloadForm>
+   <DownloadForm></DownloadForm>
     <TaskDownloads></TaskDownloads>
   </div>
 </template>
