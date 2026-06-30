9c957d7adfde6b0d969e2fb2e6548e97f9712ae1
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-06-30 10:33:17 -0500

    remove production service old struct

--- services/production/docker_build.bat
+++
@@ -1,1 +0,0 @@
-docker build -t angeluz07/cc-production:0.0.1 .
--- services/production/docker_run.bat
+++
@@ -1,1 +0,0 @@
-docker run -v "%cd%:/app/" -p 8000:8000 --env-file=.env --add-host=localhost:host-gateway -it angeluz07/cc-production:0.0.1 
