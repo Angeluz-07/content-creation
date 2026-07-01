1521ce97b14ef8393dbcc3112e33f16f547f9a77
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-07-01 18:30:43 -0500

    remove folder discovery

--- writer/domain/models.py
+++ writer/domain/models.py
@@ -24,3 +24,17 @@ class Post:
     id: str
     seed: str
     content: str
+
+
+from typing import List
+from dataclasses import dataclass, field
+from uuid import uuid4
+
+@dataclass
+class Prompt:
+    system_content: str
+    user_content: str
+    id: str 
+    name: str
+    num_predict: int = 200
+    temperature: float = 0.75
