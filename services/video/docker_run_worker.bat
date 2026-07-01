01d21633727607f297c271d4a213dd118a148d14
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-07-01 16:42:16 -0500

    remove video service old struct

--- services/video/docker_run_worker.bat
+++
@@ -1,3 +0,0 @@
-docker run -v "%cd%:/app/" --env-file=.env --add-host=localhost:host-gateway -it angeluz07/cc-video-build:0.0.1 ^
-    taskiq worker workers.broker:broker workers.video_build ^
-    --workers 2
