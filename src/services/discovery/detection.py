dd5173543eb0149b1b600a83a11f4814c2fbe3ae
R Alejandro Mena <alexandro.ramr777@gmail.com>
2026-07-08 16:26:43 -0500

    move discovery code

--- src/services/discovery/detection.py
+++ src/services/discovery/detection.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 from dataclasses import field
 from abc import ABC, abstractmethod
-from src.domain.services.discovery.vtt_parser import VTTParser
-from src.domain.services.discovery.discovery_parser import DiscoveryParser
-from src.domain.services.discovery.scanner import Scanner
+from src.domain.discovery.vtt_parser import VTTParser
+from src.domain.discovery.discovery_parser import DiscoveryParser
+from src.domain.discovery.scanner import Scanner
 from src.services.common.asset import AssetProvider
 from src.domain.common import save_json
 
@@ -28,6 +28,25 @@ class DetectorV2(BaseDetector):
         vtt_path = self.assets.get_path("vtt", input_filename)
         output_path = self.assets.get_path("metals", output_filename)
         result = self.vtt_parser.run(vtt_path, data.get("min_words"))
+        print("debug v2", result[0])
         result = self.scanner.run(result, data.get("sensitivity"))
         result = self.discovery_parser.run(result, output_filename, url)
         save_json(result, output_path)
+        return result
+
+class DetectorV3(BaseDetector):
+
+    def run(self, data, transcription_parser):
+        input_filename = data.get("input_filename")
+        output_filename = data.get("output_filename")
+        url = data.get("url")
+
+        transcription_path = self.assets.get_path("transcriptions", input_filename)
+        output_path = self.assets.get_path("metals", output_filename)
+
+        result = transcription_parser.run(transcription_path, data.get("min_words"))
+        print("debug v3", result[0])
+        result = self.scanner.run(result, data.get("sensitivity"))
+        result = self.discovery_parser.run(result, output_filename, url)
+        save_json(result, output_path)
+        return result
